60de4059b0dc45537091fb368443f53168eb197f
Devon Langendoerfer <425devon@gmail.com>
2018-05-20 09:35:07 -0700

    initialized new repository

---
+++ algorithms-1/src/com/devon/BasicOperation.java
@@ -0,0 +1,12 @@
+package com.devon;
+
+public class BasicOperation {
+    public BasicOperation() {
+    }
+
+    public void countTo(int number){
+        for(int i = 0; i <= number; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ algorithms-1/src/com/devon/Main.java
@@ -0,0 +1,9 @@
+package com.devon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BasicOperation quickTest = new BasicOperation();
+	    quickTest.countTo(255);
+    }
+}
