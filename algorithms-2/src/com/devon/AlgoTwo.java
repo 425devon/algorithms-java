aced09ac3dfc05fecdff1d6af96dbe34c88d8a2e
Devon Langendoerfer <425devon@gmail.com>
2018-05-20 15:56:38 -0700

    added recursive solutions for rFib and rFact

--- algorithms-2/src/com/devon/AlgoTwo.java
+++ algorithms-2/src/com/devon/AlgoTwo.java
@@ -3,6 +3,8 @@ package com.devon;
 public class AlgoTwo {
     public AlgoTwo() {
     }
+
+    //iterative sum
     public int iterativeSum(int number){
         int sum = 0;
         for(int i = 0; i <= number; i++){
@@ -10,5 +12,48 @@ public class AlgoTwo {
         }
         System.out.println(sum);
         return sum;
+    }
+
+    //recursive sum
+    public int recursiveSum(int num) {
+        if(num == 0){ return 0; }
+        return num + recursiveSum(--num);
+    }
+
+    //iterative factorial
+    public int iterativeFactorial(int number){
+        int sum = 1;
+        for(int i = number; i >= 1; i--){
+            sum *= i;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    //recursive factorial
+    public int recursiveFactorial(int number){
+        if(number == 1){ return 1; }
+        return number *= recursiveFactorial(--number);
+    }
+
+    //iterativeFibonacci
+    public int iterativeFibonacci(int number){
+        if(number == 0){ return 0; }
+        if(number < 3){ return 1; }
+        int prev = 1;
+        int result = 1;
+        for(int i = 3; i <= number; i++){
+            int temp = result;
+            result = result + prev;
+            prev = temp;
+        }
+        return result;
+    }
+
+    //recursive fibonacci
+    public int recusriveFibonacci(int number){
+        if(number <= 0){ return 0; }
+        if(number < 3){ return 1; }
+        return (recusriveFibonacci(number -1) + recusriveFibonacci(number - 2));
     }
 }
--- algorithms-2/src/com/devon/Main.java
+++ algorithms-2/src/com/devon/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    AlgoTwo quickTest = new AlgoTwo();
-	    quickTest.iterativeSum(5);
+	    //quickTest.iterativeSum(5);
+        //System.out.println(quickTest.recursiveSum(5));
+        //quickTest.iterativeFactorial(5);
+        //System.out.println(quickTest.recursiveFactorial(5));
+        //System.out.println(quickTest.iterativeFibonacci(5));
+        System.out.println(quickTest.recusriveFibonacci(5));
     }
+
 }
